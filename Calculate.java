f9e060c2a03bcec9cf73abd99427d24f3cd5b9b7
unknown <Amplexor2020>
2020-03-16 12:58:06 +0300

    create Calculate

--- Calculate.java
+++ Calculate.java
@@ -1,9 +1,8 @@
 public class Calculate {
 
-	public static void main(String[] arg) {
-		System.out.println("Calculate...");
-
-	}
+	public static void main (String [] args) {
+		System.out.print("Calculate...");
 
 
+	}	
 }
